93e22904c383c23d7693296066152fef658dbb4a
Guillermo-Buzzoni <guillebuzzoni@gmail.com>
2024-10-20 21:32:04 -0300

    Add redirect to login.html for routes with @login_required when accessing from browser

--- app/helpers.py
+++ app/helpers.py
@@ -1,11 +1,14 @@
 from functools import wraps
-from flask import session, jsonify
+from flask import jsonify, redirect, request, session, url_for
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if "user_id" not in session:
-            return jsonify({"error": "User not logged in"}), 401
+            if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+                return jsonify({"error": "User not logged in"}), 401
+            else:
+                return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
 
